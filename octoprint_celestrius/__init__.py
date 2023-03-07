de34caa9a6b904c3bea827f3c2bb56812429815e
Kenneth Jiang <kenneth.jiang@gmail.com>
2023-03-07 09:37:39 -0800

    Privacy and terms

--- octoprint_celestrius/__init__.py
+++ octoprint_celestrius/__init__.py
@@ -45,6 +45,7 @@ class CelestriusPlugin(octoprint.plugin.SettingsPlugin,
             'snapshot_url': 'http://localhost:8080/?action=snapshot',
             'enabled': False,
             'pilot_email': None,
+            'terms_accepted': False,
         }
 
     ##~~ AssetPlugin mixin
