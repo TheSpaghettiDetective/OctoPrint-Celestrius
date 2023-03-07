0094d29738ff6a846b2a9ac1a307a5d7b6e57746
Kenneth Jiang <kenneth.jiang@gmail.com>
2023-03-07 10:40:59 -0800

    Minor wokding

--- octoprint_celestrius/static/js/celestrius.js
+++ octoprint_celestrius/static/js/celestrius.js
@@ -12,14 +12,17 @@ $(function () {
         self.settingsViewModel = parameters[0];
         self.wizardViewModel = parameters[1];
 
-        self.isEnabled = ko.pureComputed(function() {
-            return self.settingsViewModel.settings.plugins.celestrius.enabled
-                && self.settingsViewModel.settings.plugins.celestrius.enabled();
-        })
+        self.isEnabled = ko.pureComputed(function () {
+            return (
+                self.settingsViewModel.settings.plugins.celestrius.enabled &&
+                self.settingsViewModel.settings.plugins.celestrius.enabled()
+            );
+        });
 
         // TODO: Implement your plugin's view model here.
         self.toggleIsEnabled = function () {
-            const newVal = !self.settingsViewModel.settings.plugins.celestrius.enabled();
+            const newVal =
+                !self.settingsViewModel.settings.plugins.celestrius.enabled();
             self.settingsViewModel.settings.plugins.celestrius.enabled(newVal);
             self.settingsViewModel.saveData();
         };
@@ -34,13 +37,13 @@ $(function () {
         });
         self.consentText = ko.pureComputed(function () {
             return self.termsAccepted()
-                ? "You accepted the terms for participating in the pilot program."
-                : "I understand the privacy policy and accept the terms for participating in the pilot program."
+                ? "You have accepted the terms for participating in the pilot program."
+                : "I understand the privacy policy and accept the terms for participating in the pilot program.";
         });
         self.savePluginSettings = function () {
             self.settingsViewModel.saveData();
             return true;
-        }
+        };
     }
 
     /* view model class, parameters for constructor, container to bind to
@@ -52,6 +55,10 @@ $(function () {
         // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
         dependencies: ["settingsViewModel", "wizardViewModel"],
         // Elements to bind to, e.g. #settings_plugin_celestrius, #tab_plugin_celestrius, ...
-        elements: ["#settings_plugin_celestrius", '#wizard_plugin_celestrius', "#navbar_plugin_celestrius"],
+        elements: [
+            "#settings_plugin_celestrius",
+            "#wizard_plugin_celestrius",
+            "#navbar_plugin_celestrius",
+        ],
     });
 });
